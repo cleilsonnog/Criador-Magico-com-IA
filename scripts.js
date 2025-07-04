00e0cd74a22b3cf1f49e817ff71d0950b15c77a9
cleilsonnog <cleilsonnogueira45@gmail.com>
2025-07-04 15:37:34 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,31 @@
+let webhook = "https://cleilson.app.n8n.cloud/webhook/animacao-css"
+
+
+
+
+async function cliqueiNoBotao() {
+    let textoInput = document.querySelector(".input-animacao").value
+    let codigo = document.querySelector(".area-codigo")
+    let areaResultado = document.querySelector(".area-resultado")
+
+    let resposta = await fetch(webhook, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ pergunta: textoInput })
+    })
+
+    console.log(resposta)
+    let resultado = await resposta.json()
+    
+      
+    let info = JSON.parse(resultado[0].resposta)
+
+    console.log(info)
+
+     codigo.innerHTML = info.code
+     areaResultado.innerHTML = info.preview
+    document.head.insertAdjacentHTML('beforeend', "<style>" + info.style + "</style>")
+
+    
+}
+
