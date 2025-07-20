eea8d6b8a10806e9d187a85a648d8e06b5e8028f
cleilsonnog <cleilsonnogueira45@gmail.com>
2025-07-20 20:55:48 -0300

    alt webhook

--- scripts.js
+++ scripts.js
@@ -1,31 +1,27 @@
-let webhook = "https://cleilson.app.n8n.cloud/webhook/animacao-css"
-
-
-
+let webhook = "http://localhost:5678/webhook/animacao-css";
 
 async function cliqueiNoBotao() {
-    let textoInput = document.querySelector(".input-animacao").value
-    let codigo = document.querySelector(".area-codigo")
-    let areaResultado = document.querySelector(".area-resultado")
+  let textoInput = document.querySelector(".input-animacao").value;
+  let codigo = document.querySelector(".area-codigo");
+  let areaResultado = document.querySelector(".area-resultado");
 
-    let resposta = await fetch(webhook, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ pergunta: textoInput })
-    })
+  let resposta = await fetch(webhook, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ pergunta: textoInput }),
+  });
 
-    console.log(resposta)
-    let resultado = await resposta.json()
-    
-      
-    let info = JSON.parse(resultado[0].resposta)
+  console.log(resposta);
+  let resultado = await resposta.json();
 
-    console.log(info)
+  let info = JSON.parse(resultado[0].resposta);
 
-     codigo.innerHTML = info.code
-     areaResultado.innerHTML = info.preview
-    document.head.insertAdjacentHTML('beforeend', "<style>" + info.style + "</style>")
+  console.log(info);
 
-    
+  codigo.innerHTML = info.code;
+  areaResultado.innerHTML = info.preview;
+  document.head.insertAdjacentHTML(
+    "beforeend",
+    "<style>" + info.style + "</style>"
+  );
 }
-
